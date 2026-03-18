2b0997e1d4f990ceda39d9833ceb7f7be93eaa21
crqzycat <crqzycat@gmail.com>
2026-03-18 10:50:17 +0100

    Initial commit

---
+++ src/client/java/me/smart_pearl/smart_gap/client/Smart_gapClient.java
@@ -0,0 +1,10 @@
+package me.smart_pearl.smart_gap.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Smart_gapClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
